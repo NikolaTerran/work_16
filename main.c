d4707347bca929abd00248648352f0374d6f1ff4
terran <tliu3@stuy.edu>
2018-12-06 01:06:49 -0500

    s

--- main.c
+++ main.c
@@ -12,22 +12,31 @@ int main()
 {
 	int shmd;
 	char * data;
-	char input[3];
+	char * input;
+	input = malloc(256);
 	
 	shmd = shmget(KEY, SEG_SIZE, IPC_CREAT | 0644); //not overwrite
 	data = shmat(shmd, 0 , 0);
 	if(!(*data)){printf("New segment, no data to display\n");}
-	else{printf("woo\n");}	
-	printf("data:%s\n",data);
-	fgets(data, SEG_SIZE, stdin);
-	*strchr(data, '\n') = 0;
+	else{printf("data: %s\n",data);}	
 	
+	printf("Want to change shm?[y/n]\n");
 	
+	scanf("%s",input);
+	if(!strcmp(input,"y")){
+		printf("Just type something\n");
+		scanf("%s",data);
+		//fgets(data, SEG_SIZE, stdin);
+		//*strchr(data, '\n') = 0;
+		shmdt(data);
+	}	
+	else{
+		printf("Want to delete shm?[y/n]\n");
+		scanf("%s",input);
+		if(!strcmp(input,"y")){
+			shmctl(shmd, IPC_RMID, 0);
+		}
+	}
 	
-	shmdt(data);
-	shmctl(shmd, IPC_RMID, 0);
-	
-
-		
     return 0;
 }
